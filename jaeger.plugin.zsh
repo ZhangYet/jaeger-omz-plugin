8cb8af198b56fd94c1933924c600790f32c46d1b
Zhang Ye <ye.zhang@shopee.com>
2020-06-23 17:36:33 +0800

    fix some bug

--- jaeger.plugin.zsh
+++ jaeger.plugin.zsh
@@ -4,25 +4,20 @@
 
 function jaeger() {
     emulate -L zsh
-    local action jaeger_url jaeger_prefix
+    local action url jaeger_prefix
     if [[ -n "$1" ]]; then
 	action=$1
-    elif [[ -f .jaeger-default-action ]]; then
-	action=$(cat .jaeger-default-action)
-    elif [[ -f ~/.jaeger-default-action ]]; then
-	action=$(cat ~/.jaeger-default-action)
-    elif [[ -n "${JAEGER_DEFAULT_ACTION}" ]]; then
-	action=${JAEGER_DEFAULT_ACTION}
     else
 	action="trace"
     fi
 
-    if [[ -f .jaeger-url ]]; then
-	jaeger-url=$(cat .jaeger-url)
+    
+    if [[ -f ~/.jaeger-url ]]; then
+	url=$(cat ~/.jaeger-url)
     fi # TODO add help
 
     if [[ $action == "trace" ]]; then
-	echo "Opening new trace"
-	open_command "${jaeger-url}/trace/$2"
-    if
+	echo "Opening new trace: ${url}/trace/$2"
+	open_command "${url}/trace/$2"
+    fi
 }
